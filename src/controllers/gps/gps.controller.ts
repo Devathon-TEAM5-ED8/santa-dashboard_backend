e6933826f3e0254715695e345b396d66debe0dc2
Santiago Orozco <93175401+santiagoeor@users.noreply.github.com>
2024-11-13 18:20:41 -0500

    Update gps.controller.ts adding try, catch

--- src/controllers/gps/gps.controller.ts
+++ src/controllers/gps/gps.controller.ts
@@ -5,119 +5,141 @@ export class GpsController {
 
     async createGps(req: Request, res: Response) {
 
-        let params = req.body;
+        try {
+            let params = req.body;
 
-        // return res.status(200).json(params.latitude);
+            // return res.status(200).json(params.latitude);
 
-        if (!params.address || !params.searchDate) {
-            return res.status(422).json({
-                status: 'error',
-                message: 'Faltan datos por enviar',
-            });
-        } else {
-            console.log(params);
-            const { latitude, longitude, address, searchDate } = params;
-            const newGps = Gps.create({ latitude, longitude, address, searchDate });
-            await newGps.save();
+            if (!params.address || !params.searchDate) {
+                return res.status(422).json({
+                    status: 'error',
+                    message: 'Faltan datos por enviar',
+                });
+            } else {
+                console.log(params);
+                const { latitude, longitude, address, searchDate } = params;
+                const newGps = Gps.create({ latitude, longitude, address, searchDate });
+                await newGps.save();
 
-            return res.status(200).json({
-                status: 'success',
-                message: 'Gps creado',
-                newGps
-            });
+                return res.status(200).json({
+                    status: 'success',
+                    message: 'Gps creado',
+                    newGps
+                });
 
+            }
+
+        } catch (error) {
+            return res.status(500).json({ message: 'Error al guardar la ubicacion gps', error });
         }
 
     }
 
     async getAllGps(req: Request, res: Response) {
-        const gps = await Gps.find();
-        return res.status(200).json({
-            status: 'success',
-            gps
-        });
-    }
-
-    async getGpsById(req: Request, res: Response) {
-
-        const url = req.path;
-        const query = url.split('/');
-        const id = query[2];
-
-        if (!id) {
-            return res.status(422).json({
-                status: 'error',
-                message: 'debe enviar el id'
-            });
-        } else {
-            const gps = await Gps.findOneBy({ id: parseInt(id) });
-
+        try {
+            const gps = await Gps.find();
             return res.status(200).json({
                 status: 'success',
                 gps
             });
+        } catch (error) {
+            return res.status(500).json({ message: 'Error al obtener el listado de ubicaciones', error });
+        }
+    }
+
+    async getGpsById(req: Request, res: Response) {
+
+        try {
+            const url = req.path;
+            const query = url.split('/');
+            const id = query[2];
+
+            if (!id) {
+                return res.status(422).json({
+                    status: 'error',
+                    message: 'debe enviar el id'
+                });
+            } else {
+                const gps = await Gps.findOneBy({ id: parseInt(id) });
+
+                return res.status(200).json({
+                    status: 'success',
+                    gps
+                });
+            }
+        } catch (error) {
+            return res.status(500).json({ message: 'Error al obtener la ubicación', error });
         }
 
     }
 
     async updateGps(req: Request, res: Response) {
 
-        const url = req.path;
-        const query = url.split('/');
-        const id = query[2];
+        try {
+            const url = req.path;
+            const query = url.split('/');
+            const id = query[2];
 
-        if (!id) {
-            return res.status(422).json({
-                status: 'error',
-                message: 'debe enviar el id'
-            });
-        } else {
-            const gps = await Gps.findOneBy({ id: parseInt(id) });
-
-            if (!gps) {
-                return res.status(404).json(
-                    { message: 'Gps no encontrado' }
-                );
-            } else {
-
-                Object.assign(gps, req.body);
-                await gps.save();
-
-                return res.status(200).json({
-                    status: 'success',
-                    message: 'Gps editado',
-                    gps
+            if (!id) {
+                return res.status(422).json({
+                    status: 'error',
+                    message: 'debe enviar el id'
                 });
+            } else {
+                const gps = await Gps.findOneBy({ id: parseInt(id) });
 
+                if (!gps) {
+                    return res.status(404).json(
+                        { message: 'Gps no encontrado' }
+                    );
+                } else {
+
+                    Object.assign(gps, req.body);
+                    await gps.save();
+
+                    return res.status(200).json({
+                        status: 'success',
+                        message: 'Gps editado',
+                        gps
+                    });
+
+                }
             }
+
+        } catch (error) {
+            return res.status(500).json({ message: 'Error al editar la ubicación gps', error });
         }
 
     }
 
     async deleteGps(req: Request, res: Response) {
 
-        const url = req.path;
-        const query = url.split('/');
-        const id = query[2];
+        try {
+            const url = req.path;
+            const query = url.split('/');
+            const id = query[2];
 
-        if (!id) {
-            return res.status(422).json({
-                status: 'error',
-                message: 'debe enviar el id'
-            });
-        } else {
-            const gps = await Gps.findOneBy({ id: parseInt(id) });
-            if (!gps) {
-                return res.status(404).json(
-                    { message: 'Gps no encontrado' }
-                );
+            if (!id) {
+                return res.status(422).json({
+                    status: 'error',
+                    message: 'debe enviar el id'
+                });
             } else {
+                const gps = await Gps.findOneBy({ id: parseInt(id) });
+                if (!gps) {
+                    return res.status(404).json(
+                        { message: 'Gps no encontrado' }
+                    );
+                } else {
 
-                await gps.remove();
+                    await gps.remove();
 
-                return res.status(204).send();
+                    return res.status(204).send();
 
+                }
             }
+        } catch (error) {
+            return res.status(500).json({ message: 'Error al eliminar la ubicación gps', error });
         }
 
     }
